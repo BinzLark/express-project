351550c137a0f2efd871dde220bd9dfe7693251a
Phan Viet Tan <nhoctargunn@gmail.com>
2020-04-04 21:40:49 +0700

    Add user controller

---
+++ controller/user.controller.js
@@ -0,0 +1,62 @@
+const db = require('../db');
+const shortid = require('shortid');
+
+
+module.exports.index = function (req, res) {
+    let users = db.get('users').value();
+    let search = req.query.search;
+    let newUser = [];
+    if (search)
+        newUser = users.filter(user => user.name.toLowerCase().indexOf(search.toLowerCase()) != -1);
+    console.log(search);
+    res.render('users/index', {
+        users: newUser.length > 0 ? newUser : users,
+        search: search ? search : ''
+    });
+};
+
+module.exports.create = (req, res) => {
+    res.render('users/create');
+};
+
+module.exports.postCreate = (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users')
+    // res.json(req.body)
+};
+
+module.exports.view = function (req, res) {
+    let id = req.params.id;
+    let user = db.get('users').find({id}).value();
+    res.render('users/detail', {user})
+};
+
+
+// module.exports = {
+//     index(req, res) {
+//         let users = db.get('users').value();
+//         let search = req.query.search;
+//         let newUser = [];
+//         if (search)
+//             newUser = users.filter(user => user.name.toLowerCase().indexOf(search.toLowerCase()) != -1);
+//         res.render('users/index', {
+//             users: newUser.length > 0 ? newUser : users,
+//             search: search ? search : ''
+//         });
+//     },
+//
+//     create(req, res) {
+//         res.render('users/create');
+//     },
+//
+//     postCreate(req, res) {
+//         req.body.id = shortid.generate();
+//         db.get('users').push(req.body).write();
+//         res.redirect('/users')
+//     },
+//     view(req, res) {//         let id = req.params.id;
+//         let user = db.get('users').find({id}).value();
+//         res.render('users/detail', {user})
+//     }
+// };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,38 +1,11 @@
 const express = require('express');
-const shortid = require('shortid');
-const db = require('../db');
+const controller = require('../controller/user.controller');
+
 const router = express.Router();
 
-var users = db.get('users').value();
-
-router.get('/', function (req, res) {
-    let search = req.query.search;
-    let newUser = [];
-    if (search)
-        newUser = users.filter(user => user.name.toLowerCase().indexOf(search.toLowerCase()) != -1);
-    console.log(search);
-    res.render('users/index', {
-        users: newUser.length > 0 ? newUser : users,
-        search: search ? search : ''
-    });
-});
-
-router.get('/create', (req, res) => {
-    res.render('users/create');
-});
-
-router.post('/create', (req, res) => {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-    // res.json(req.body)
-});
-
-
-router.get('/:id', function (req, res) {
-    let id = req.params.id;
-    let user = db.get('users').find({ id }).value();
-    res.render('users/detail', { user })
-});
+router.get('/', controller.index);
+router.get('/create', controller.create);
+router.post('/create', controller.postCreate);
+router.get('/:id', controller.view);
 
 module.exports = router;
