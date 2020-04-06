26eb91b2606225f0de6673e3f1d549d7710389ce
Phan Viet Tan <nhoctargunn@gmail.com>
2020-04-06 21:56:48 +0700

    upload avatar with npm multer

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,19 @@
 const express = require('express');
+var multer = require('multer')
+
 const controller = require('../controller/user.controller');
 const middleware = require('../middleware/user.middleware');
 
 const router = express.Router();
+var upload = multer({ dest: './public/uploads/' })
 
 router.get('/', controller.index);
 router.get('/create', controller.create);
-router.post('/create', middleware.postCreate, controller.postCreate);
+router.post('/create',
+    upload.single('avatar'),
+    middleware.postCreate,
+    controller.postCreate
+);
 router.get('/:id', controller.view);
 
 module.exports = router;
